5cb356fb375517ed2bfc401c81977e35117c5eee
johni1228 <akirus1228@gmail.com>
2021-12-04 10:30:30 +0900

    fix:Response currency component

--- src/components/Currency/index.tsx
+++ src/components/Currency/index.tsx
@@ -8,17 +8,19 @@ interface Props {
 
 export const Currency = ({currency}: Props) => {
   return (<>
-    <div className="pb-10 border-b-2 mb-10 flex msm:flex-col sm:flex-row sm:items-center">
-      <div className="flex justify-start items-center flex-grow-0 sm:w-1/2">
-        <TokenImage token={currency} width={30} height={30} />
-        <div className="flex flex-col justify-center items-center ml-20">
-          <p className="font-bold text-20">{currency.symbol}</p>
-          <p className="msm:hidden sm:block">{currency.name}</p>
+    <div className="pb-10 border-b-2 mb-10 flex msm:flex-col sm:grid sm:grid-cols-3 sm:items-center">
+      <div className="flex justify-start items-center flex-grow-0">
+        <TokenImage token={currency} min-width={30} min-height={30} />
+        <div className="flex flex-col justify-center items-center ml-30">
+          <p className="font-bold md:text-20 msm:text-15">{currency.symbol}</p>
+          <p className="msm:hidden md:block text-center">{currency.name}</p>
         </div>
       </div>
-      <div className="flex justify-between flex-grow">
-        <p>{currency.amount} BNB</p>
-        <p className="cursor-pointer">view</p>
+      <p className="msm:hidden sm:flex justify-center">{currency.amount} BNB</p>
+      <p className="msm:hidden sm:flex cursor-pointer justify-end">view</p>
+      <div className="msm:flex msm:justify-between msm:flex-grow sm:hidden">
+        <p className="flex justify-start">{currency.amount} BNB</p>
+        <p className="cursor-pointer flex justify-end">view</p>
       </div>
     </div>
 
--- src/pages/Tokens/index.tsx
+++ src/pages/Tokens/index.tsx
@@ -33,17 +33,17 @@ const Tokens = () => {
 
   return (
     <Page color="transparent" >
-      <SearchBar className="flex msm:flex-col sm:flex-row sm:items-center justify-around sm: mb-100 msm:mb-50">
+      <SearchBar className="flex msm:flex-col sm:flex-row sm:items-center justify-around sm: mb-100 msm:mb-50 min-h-full">
           <div className="msm:min-w-min sm:w-900 sm:mb-10 msm:mb-5"><input type="text" name="searchbar" id="searchbar" placeholder="Search by token address" className="outline-none p-10 border-2 w-full rounded-xl border-gray-900"/></div>
           <div className="flex msm:justify-between sm:justify-around items-center flex-grow">
             <Text onClick={() => setAll(true)} className="msm:w-80 md:120">All</Text>
             <Text onClick={() => setAll(false)} className="msm:w-80 md:120">My Lock</Text>
           </div>
       </SearchBar>
-      <CurrenciesHeader className="msm:hidden sm:block flex justify-between items-center mb-30">
-        <div className="w-100 text-center font-bold">TOKEN</div>
-        <div className="text-center font-bold">Amount</div>
-        <div className="font-bold">VIEW</div>
+      <CurrenciesHeader className="msm:hidden sm:grid grid grid-cols-3 text-center mb-30">
+        <div className="text-center font-bold flex justify-start">TOKEN</div>
+        <div className="text-center font-bold justify-center">Amount</div>
+        <div className="font-bold flex justify-end">VIEW</div>
       </CurrenciesHeader>
       <Currencies currencies={currencies}/>
     </Page>
